5c416ef3d352cd38f798ca208c06917b4d79241a
thecoder-droid <thegabrielfrancis@gmail.com>
2020-06-20 16:43:00 -0400

    message

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-let starWarsPeopleList = document.querySelector('ul');
+let starWarsPeople = document.querySelector('p');
 
 fetch('https://swapi.dev/api/people/')
 .then(function(response) { 
@@ -9,11 +9,37 @@ fetch('https://swapi.dev/api/people/')
     let people = json.results;
 
     for(p of people) {
-        let listItem = document.createElement('li'); 
-        // listItem.innerHTML = '<p>' + p.name +  ':' + 'height: ' + p.height + p.mass + '</p>';  
-        listItem.innerHTML = `<p>  </p>`
-        listItem.innerHTML = `<p> ${p.name}: <br> birthyear: ${p.birth_year} <br> eye color: ${p.eye_color} <br> gender: ${p.gender} <br> hair color: ${p.hair_color} <br> height: ${p.height} <br> mass: ${p.mass} <br> skin color: ${p.skin_color} <br> homeworld: ${p.homeworld} <br> films: ${p.films} <br> species: ${p.species} <br> starships: ${p.starships} <br> vehicles: ${p.vehicles} <br> url: ${p.url} </p>`;
-        starWarsPeopleList.appendChild(listItem);  
+        let name = document.createElement('p');
+        let birth_year = document.createElement('p');
+        let eye_color = document.createElement('p');
+        let gender = document.createElement('p');
+        let hair_color = document.createElement('p');
+        let height = document.createElement('p');
+        let mass = document.createElement('p');
+        let skin_color = document.createElement('p');
+        let starWarsPeopleList = document.createElement('div');
+        starWarsPeopleList.className = 'border';
+
+
+        name.innerHTML = '<p>' + p.name + '</p>';
+        birth_year.innerHTML = '<p>' + 'birth year: ' + p.birth_year + '</p>';
+        eye_color.innerHTML = '<p>' + 'eye color: ' + p.eye_color + '</p>';
+        gender.innerHTML = '<p>' + 'gender: ' + p.gender + '</p>';
+        hair_color.innerHTML = '<p>' + 'hair color: ' + p.hair_color + '</p>';
+        height.innerHTML = '<p>' + 'height: ' + p.height + '</p>';
+        mass.innerHTML = '<p>' + 'mass: ' + p.mass + '</p>';
+        skin_color.innerHTML = '<p>' + 'skin color: ' + p.skin_color + '</p>';
+
+        starWarsPeople.appendChild(starWarsPeopleList);
+        starWarsPeopleList.appendChild(name).style.color='yellow'; 
+        starWarsPeopleList.appendChild(name).style.fontSize='2em'; 
+        starWarsPeopleList.appendChild(birth_year);
+        starWarsPeopleList.appendChild(eye_color);
+        starWarsPeopleList.appendChild(gender);
+        starWarsPeopleList.appendChild(hair_color);
+        starWarsPeopleList.appendChild(height);
+        starWarsPeopleList.appendChild(mass);
+        starWarsPeopleList.appendChild(skin_color); 
       }
 });
 
