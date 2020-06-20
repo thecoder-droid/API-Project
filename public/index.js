901dba8cd487190c80a0fa290ae06600fc37897d
thecoder-droid <thegabrielfrancis@gmail.com>
2020-06-20 14:01:22 -0400

    message

--- public/index.js
+++ public/index.js
@@ -11,7 +11,8 @@ fetch('https://swapi.dev/api/people/')
     for(p of people) {
         let listItem = document.createElement('li'); 
         // listItem.innerHTML = '<p>' + p.name +  ':' + 'height: ' + p.height + p.mass + '</p>';  
-        listItem.innerHTML = `<p> name: ${p.name}: <br> birthyear: ${p.birth_year} <br> eye color: ${p.eye_color} <br> gender: ${p.gender} <br> hair color: ${p.hair_color} <br> height: ${p.height} <br> mass: ${p.mass} <br> skin color: ${p.skin_color} <br> homeworld: ${p.homeworld} <br> films: ${p.films} <br> species: ${p.species} <br> starships: ${p.starships} <br> vehicles: ${p.vehicles} <br> url: ${p.url} </p>`;
+        listItem.innerHTML = `<p>  </p>`
+        listItem.innerHTML = `<p> ${p.name}: <br> birthyear: ${p.birth_year} <br> eye color: ${p.eye_color} <br> gender: ${p.gender} <br> hair color: ${p.hair_color} <br> height: ${p.height} <br> mass: ${p.mass} <br> skin color: ${p.skin_color} <br> homeworld: ${p.homeworld} <br> films: ${p.films} <br> species: ${p.species} <br> starships: ${p.starships} <br> vehicles: ${p.vehicles} <br> url: ${p.url} </p>`;
         starWarsPeopleList.appendChild(listItem);  
       }
 });
